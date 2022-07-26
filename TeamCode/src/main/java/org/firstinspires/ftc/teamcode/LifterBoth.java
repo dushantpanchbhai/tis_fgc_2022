4533f148f5af05cc74d1b32282b6fc57efbf7863
dushantpanchbhai <dushantrpanchbhai@gmail.com>
2022-07-26 16:44:57 +0530

    partiam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBoth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBoth.java
@@ -2,48 +2,55 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 
 @Config
-@TeleOp(name = "joystick control",group = "control")
-public class JoystickControl extends LinearOpMode {
+@TeleOp(name = "Lifter Both",group = "control")
+public class LifterBoth extends LinearOpMode {
 
-    DcMotorEx lifter1,lifter2,intake1,intake2;
-    public static double intake_power = 1;
+    DcMotorEx lifter1,lifter2;
     public static int inc = 50;
-    public int pos = 0;
 
     public static int factor1 = 1;
     public static int factor2 = 1;
 
+    public static boolean reverse = false;
+
     @Override
     public void runOpMode() throws InterruptedException {
-
-        SampleTankDrive drive = new SampleTankDrive(hardwareMap);
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        intake1 = hardwareMap.get(DcMotorEx.class,"intake1");
-        intake2 = hardwareMap.get(DcMotorEx.class,"intake2");
-        intake1.setPower(0);
-        intake2.setPower(0);
 
         lifter1 = hardwareMap.get(DcMotorEx.class,"lifter1");
         lifter1.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         lifter1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        PIDFCoefficients x = new PIDFCoefficients(3,5,6,6);
+        lifter1.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION,x);
 
         lifter1.setTargetPosition(0);
         lifter1.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         lifter1.setPower(1);
 
 
+
+
         lifter2 = hardwareMap.get(DcMotorEx.class,"lifter2");
-        lifter2.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+//        lifter2.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         lifter2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        if(reverse == true)
+        {
+            lifter2.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
 
         lifter2.setTargetPosition(0);
         lifter2.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
@@ -53,52 +60,27 @@ public class JoystickControl extends LinearOpMode {
 
         while(opModeIsActive())
         {
-
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            gamepad1.left_stick_y,
-                            0,
-                            -gamepad1.right_stick_x
-                    )
-            );
-            drive.update();
-
             if(gamepad1.y)
             {
-                pos = pos + inc;
-                moveTo(pos);
+                lifter1.setTargetPosition(lifter1.getCurrentPosition() + inc);
             }
             else if(gamepad1.b)
             {
-                pos = pos - inc;
-                moveTo(pos);
+                lifter1.setTargetPosition(lifter1.getCurrentPosition() - inc);
             }
 
-            if(gamepad1.right_bumper)
+            if(gamepad1.x)
             {
-                intake1.setPower(intake_power);
-                intake2.setPower(-intake_power);
+                lifter2.setTargetPosition(lifter2.getCurrentPosition() + inc);
             }
-            else if(gamepad1.left_bumper)
+            else if(gamepad1.a)
             {
-                intake1.setPower(-intake_power);
-                intake2.setPower(intake_power);
-            }
-            else
-            {
-                intake1.setPower(0);
-                intake2.setPower(0);
+                lifter2.setTargetPosition(lifter2.getCurrentPosition() - inc);
             }
 
             telemetry.addData("lifter1 position",lifter1.getCurrentPosition());
             telemetry.addData("lifter2 position",lifter2.getCurrentPosition());
             telemetry.update();
         }
-    }
-
-    void moveTo(int pos)
-    {
-        lifter1.setTargetPosition(factor1*pos);
-        lifter2.setTargetPosition(factor2*pos);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Servo test",group = "servo")
+public class ServoTest extends LinearOpMode {
+
+    Servo holder;
+
+    public static double inc = 0.01;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        holder = hardwareMap.get(Servo.class,"holder");
+
+        waitForStart();
+        while (opModeIsActive())
+        {
+            if(gamepad1.y)
+            {
+                holder.setPosition(holder.getPosition() + inc);
+            }
+            else if(gamepad1.b)
+            {
+                holder.setPosition(holder.getPosition() - inc);
+            }
+
+            telemetry.addData("servo position ",holder.getPosition());
+            telemetry.update();
+        }
+    }
+}
